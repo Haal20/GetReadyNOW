92d944d551cd742ccc2c0e7d346702616eabb622
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-11-18 16:52:48 +0100

    skapade enhetstest-fil

---
+++ test/getreadynow.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+
+namespace getreadynow.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var value = "2";
+            Assert.Equal("2", value);
+        }
+    }
+}
